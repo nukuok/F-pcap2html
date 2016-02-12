faaf51bc599ec3c2d1fb2e3d14143d85ee9c1de6
nukuok <hyy314159@gmail.com>
2016-02-12 19:16:04 +0900

    update

--- ip-header.lisp
+++ ip-header.lisp
@@ -12,6 +12,13 @@
 	(222 231 228 12)
 	(222 231 228 59)))
 
+(defvar *ip-header-list-v4-3*)
+(setf *ip-header-list-v4-3*
+      '((10 20 91 5)
+	()
+	()
+	()))
+
 (defvar *ip-header-list-v6*)
 (setf *ip-header-list-v6*
       '((32 1 167 255 33 0 0 13 0 0 0 0 0 0 240 1)
@@ -19,10 +26,20 @@
 	(32 1 167 255 33 0 0 12 0 0 0 0 0 0 0 17)
 	(32 1 167 255 33 0 0 12 0 0 0 0 0 0 240 1)))
 
+(defvar *ip-header-list-v6-2*)
+(setf *ip-header-list-v6-2*
+      '((32 0 0 2 0 0 0 0 0 0 0 0 0 145 0 5)
+	(32 1 167 255 33 0 0 13 0 0 0 0 0 0 0 18)
+	()
+	()))
+	
+
 (defvar *ip-header-list* nil)
 (setf *ip-header-list*
       (list
        *ip-header-list-v4*
        *ip-header-list-v4-2*
+       *ip-header-list-v4-3*
        *ip-header-list-v6*
+       *ip-header-list-v6-2*
        ))
